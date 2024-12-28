49fd8fedc29716b6a6466e22076daebc30531de2
Guy MacGeorges <fabien@themonkeyparadox.com>
2024-12-28 15:49:33 +0100

    UI Prefab WiP

---
+++ Assets/Scripts/Panels/Weather/WeatherForecastUIElement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WeatherForecastUIElement : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text dateTime;
+    [SerializeField]
+    private TMP_Text temperature;
+    [SerializeField]
+    private TMP_Text pressure;
+
+    public void Initialize( string newDateTime, string newTemperature, string newPressure)
+    {
+        dateTime.text = newDateTime;
+        pressure.text = newTemperature;
+        temperature.text = newPressure;
+    }
+}
